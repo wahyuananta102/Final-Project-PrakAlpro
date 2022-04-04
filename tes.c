6de9ef6c4d4ef39b5416d75fb1f68af8dfc1bb1c
Gusnand <72637679+Gusnand@users.noreply.github.com>
2022-04-04 23:55:56 +0800

    Add files via upload

---
+++ tes.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    
+    printf("Program Membuat Inventori");
+    switch (pilihan)
+    {
+    case 1:
+        
+        break;
+    
+    default:
+        break;
+    }
+    
+    /*
+    char data[50];
+    int nim;
+    FILE *fp;
+
+    fp = fopen ("tes.txt", "w");
+    for (int i=0; i<5; i++)
+    {
+        printf("Masukkan nama : ");
+        fflush(stdin);
+        gets(data);
+        fprintf(fp, "%s\n", data);
+        printf("Masukkan NIM : ");
+        scanf("%d", &nim);
+        fprintf(fp, "%d\n", nim);
+    }
+    fclose(fp);
+    printf("\nProses Berhasil Ditulis.");
+    */
+    return 0;
+}
